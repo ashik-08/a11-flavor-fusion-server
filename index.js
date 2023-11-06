4f704d6008eac8a9bda01a07285d650ecb1f6a0d
ashik-08 <ashikurrahman.8october@gmail.com>
2023-11-06 20:53:06 +0600

    added POST method to add new user to database using /add-user API

--- index.js
+++ index.js
@@ -6,7 +6,12 @@ const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const port = process.env.PORT || 5001;
 
 // middleware
-app.use(cors());
+app.use(
+  cors({
+    origin: ["http://localhost:5173"],
+    credentials: true,
+  })
+);
 app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ya8cack.mongodb.net/?retryWrites=true&w=majority`;
@@ -24,6 +29,34 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    // connect to the database & access it's collections
+    const database = client.db("flavor-fusion");
+    const usersCollection = database.collection("users");
+    const foodItemsCollection = database.collection("food-items");
+
+    // user related API (usersCollection)
+    // add new user credentials to the db
+    app.post("/api/v1/add-user", async (req, res) => {
+      try {
+        const user = req.body;
+        // query to find all users in the collection
+        const query = await usersCollection.find().toArray();
+        // check if there already exist an user
+        const found = query.find(
+          (search) => search.name === user.name && search.email === user.email
+        );
+        if (found) {
+          return res.send({ message: "Already exists" });
+        }
+        const result = await usersCollection.insertOne(user);
+        res.send(result);
+      } catch (error) {
+        console.log(error);
+        return res.send({ error: true, message: error.message });
+      }
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
